992925a8911b0ab4a650309ce7998ffab91eedb9
Federico Nicolas Zamora <federicozamora616@gmail.com>
2022-11-01 16:58:59 -0300

    inicio actualizado

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -1,64 +1,66 @@
-
+import "./home.css"
 const Home = (parametros) => {
-  if(parametros.home === false){
-    return (
-      <>
-      </>
-    );
+  if (parametros.home === false) {
+    return <></>;
   }
   return (
     <>
-      <section className="jumbotron text-center">
-        <div className="container">
-          <h1 className="cover-heading pt-4">MiniMarket Matias</h1>
-          <p className="lead">De La Puente a tu carrito</p>
-        </div>
-      </section>
-      <div className="albun py-5">
-        <div className="row">
-          <div className="col-md-4 pb-2 ">
-            <div className="card md-4 alert alert-dark border border-secondary rounded-to">
-            <h3 >noticias</h3>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloremque, eum praesentium unde deleniti necessitatibus facilis error culpa? Temporibus facere nulla odio provident, dolor totam explicabo eos ex voluptates commodi!</p>
-            </div>
-          </div>
-          <div className="col-md-4 pb-2">
-            <div className="card md-4 alert alert-dark border border-secondary rounded-to">
-              <h3 >noticias</h3>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloremque, eum praesentium unde deleniti necessitatibus facilis error culpa? Temporibus facere nulla odio provident, dolor totam explicabo eos ex voluptates commodi!</p>
-            </div>
-          </div>
-          <div className="col-md-4 pb-2">
-            <div className="card md-4 alert alert-dark border border-secondary rounded-to">
-              <h3 >noticias</h3>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloremque, eum praesentium unde deleniti necessitatibus facilis error culpa? Temporibus facere nulla odio provident, dolor totam explicabo eos ex voluptates commodi!</p>
-            </div>
-          </div>
-          <div className="col-md-4 pb-2">
-            <div className="card md-4 alert alert-dark border border-secondary rounded-to">
-            <h3 >noticias</h3>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloremque, eum praesentium unde deleniti necessitatibus facilis error culpa? Temporibus facere nulla odio provident, dolor totam explicabo eos ex voluptates commodi!</p>
-            </div>
-          </div>
-          <div className="col-md-4 pb-2">
-            <div className="card md-4 alert alert-dark border border-secondary rounded-to">
-              <h3 >noticias</h3>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloremque, eum praesentium unde deleniti necessitatibus facilis error culpa? Temporibus facere nulla odio provident, dolor totam explicabo eos ex voluptates commodi!</p>
-            </div>
-          </div>
-          <div className="col-md-4 pb-2">
-            <div className="card md-4 alert alert-dark border border-secondary rounded-to">
-              <h3 >noticias</h3>
-              <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia doloremque, eum praesentium unde deleniti necessitatibus facilis error culpa? Temporibus facere nulla odio provident, dolor totam explicabo eos ex voluptates commodi!</p>
-            </div>
-          </div>
-
-        </div>
-
-
+      <div className="col-md-8 offset-md-2 info">
+        <h1 className="text-center">MiniMarket Matias</h1>
+        <p className="text-center">
+            De la Puente a tu carrito
+        </p>
       </div>
+      <section id="section">
+        <div className="container">
+          <div className="row mb-5">
+            <div className="col-8 mx-auto text-center">
+                <h2>Noticias</h2>
+                <p>somos una empresa dedidaca a la venta de comidita</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-sm-6">
+                <div className="service card-effect">
+                  <h5 className="mt-4 mb-2">service</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laboriosam. Repudiandae aliquam aut </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+                <div className="service card-effect">
+                  <h5 className="mt-4 mb-2">service</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laboriosam. Repudiandae aliquam aut </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+                <div className="service card-effect">
+                  <h5 className="mt-4 mb-2">service</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laboriosam. Repudiandae aliquam aut lordo legislattion </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+                <div className="service card-effect">
+                  <h5 className="mt-4 mb-2">service</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laboriosam. Repudiandae aliquam aut atium emperor final </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-sm-6">
+                <div className="service card-effect">
+                  <h5 className="mt-4 mb-2">service</h5>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, laboriosam. Repudiandae aliquam autv  saajlje imperium latinum </p>
+              </div>
+            
+            
+            </div>
+            
+          </div>
+
+        </div>
+
+      </section>
     </>
   );
 };
 
 export { Home };
+
--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -1,58 +1,82 @@
-
-
 const Navbar = (parametros) => {
+  const inicio = async () => {
+    parametros.setHome(true);
+    parametros.setCaja(false);
+    parametros.setStock(false);
+    parametros.setBloqueado(false);
+  };
 
-const inicio = async () => {
-  parametros.setHome(true);
-  parametros.setCaja(false);
-  parametros.setStock(false);
-};
+  const roles = parametros.credencial.roles;
 
-const caja = async () => {
-  roles.map((rol) => rol.rolId === 1 || rol.rolId === 2 ? parametros.setCaja(true) : parametros.setCaja(false))
-  parametros.setHome(false);
-  parametros.setStock(false);
- ;
-};
-const roles = parametros.credencial.roles;
-const stock = async () => {
-  parametros.setHome(false);
-  parametros.setCaja(false);
-  roles.map((rol) => rol.rolId === 1|| rol.rolId === 3 ? parametros.setStock(true) : parametros.setStock(false))
-  
-};
+  const caja = async () => {
+    const permisos = roles.find(rol => rol.rolId === 2 || rol.rolId === 1)
+    if(permisos){
+      parametros.setHome(false);
+      parametros.setCaja(true);
+      parametros.setStock(false);
+      parametros.setBloqueado(false);
+      return;
+    }
+      parametros.setHome(false);
+      parametros.setCaja(false);
+      parametros.setStock(false);
+      parametros.setBloqueado(true);
+  };
 
+  const stock = async () => {
+    const permisos = roles.find(rol => rol.rolId === 3 || rol.rolId === 1)
+    if(permisos){
+      parametros.setHome(false);
+      parametros.setCaja(false);
+      parametros.setStock(true);
+      parametros.setBloqueado(false);
+      return;
+    }
+      parametros.setHome(false);
+      parametros.setCaja(false);
+      parametros.setStock(false);
+      parametros.setBloqueado(true);
+  };
 
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark box-shadow">
         <div className="container-fluid">
-         
-          <button className="btn btn-outline-primary m-1" onClick={() => inicio()}>
+
+        
+          <button
+            className="btn btn-outline-primary m-1"
+            onClick={() => inicio()}
+          >
             Inicio
           </button>
-          <button className="btn btn-outline-primary m-1" onClick={() => caja()}>
+          <button
+            className="btn btn-outline-primary m-1"
+            onClick={() => caja()}
+          >
             Caja
           </button>
-          <button className="btn btn-outline-primary m-1" onClick={() => stock()}>
+          <button
+            className="btn btn-outline-primary m-1"
+            onClick={() => stock()}
+          >
             Stock
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
 
-            </ul>
-
-
-              <p className="text-white bg-dark p-2">Hola {parametros.credencial.nombre} !</p>
-                <button className="btn btn-outline-danger"
-                    onClick={() => {
-                        parametros.setCredencial(null);
-                    }}
-                    >
-                    Salir
-                </button>
-                
+            <h5 className="text-white bg-dark p-2">
+              Hola {parametros.credencial.nombre} !
+            </h5>
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => {
+                parametros.setCredencial(null);
+              }}
+            >
+              Salir
+            </button>
           </div>
         </div>
       </nav>
