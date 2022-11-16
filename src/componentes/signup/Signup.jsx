958b3604a9cada89fb9e15d3f48061bc3fbcf03d
Federico Nicolas Zamora <federicozamora616@gmail.com>
2022-11-16 12:30:01 -0300

    se agrego la funcion -agregar rango-

--- src/componentes/signup/Signup.jsx
+++ src/componentes/signup/Signup.jsx
@@ -14,6 +14,8 @@ const Signup = (parametros) => {
   const [fecha_nacimiento, setFecha_nacimiento] = useState("");
   const [fecha_ingreso, setFecha_ingreso] = useState("");
   const [showModalUsuarioGuardar, setShowModalUsuarioGuardar] = useState(true);
+  const [showModalRol, setShowModalRol] = useState(true)
+  const [showResitro, setShowRegistro] =useState(true)
 
   const modalUsuarios = (usuario) => {
     setShowModalUsuarioGuardar(false);
@@ -24,6 +26,43 @@ const Signup = (parametros) => {
     setFecha_ingreso(usuario.fecha_ingreso);
   };
 
+  const modalRol = (usuario)=>{
+    setShowModalRol(false)
+    setDni(usuario.dni)
+  }
+  const  mostrarRegistrar = ()=>{
+    setShowRegistro(false)
+  }
+  const singnUpRol = async (dni,rol) =>{
+   let data = {
+    dni: document.getElementById("dni-rol").value,
+    rol: document.getElementById("rol").value
+  }
+  console.log(data)
+  await apiPost("rol", data)
+    data={
+      dni,
+      rol
+    }
+  setShowModalRol(true)
+  }
+  const guardarRol=async(
+    dni,rol
+  ) =>{
+    let data = {
+      dni:document.getElementById("dni-rol").value,
+      rol:document.getElementById("rol").value
+    }
+    console.log(data)
+    await apiPut("rol", data)
+    data={
+      dni,
+      rol
+    }
+  setShowModalRol(true)
+  }
+  
+
   const guardarUsuario = async (
     dni,
     nombre,
@@ -31,14 +70,13 @@ const Signup = (parametros) => {
     fecha_nacimiento,
     fecha_ingreso
   ) => {
-    
     let data = {
       dni: document.getElementById("dni").value,
       nombre: document.getElementById("nombre").value,
       apellido: document.getElementById("apellido").value,
       fecha_nacimiento: document.getElementById("fecha_naci").value,
       fecha_ingreso: document.getElementById("fecha_ingreso").value,
-    }
+    };
     console.log(data);
     if (
       nombre !== "" &&
@@ -46,19 +84,18 @@ const Signup = (parametros) => {
       fecha_ingreso !== "" &&
       fecha_nacimiento !== ""
     ) {
-    await apiPut("usuarios", data);
-    data = {
-      dni,
-      nombre,
-      apellido,
-      contraseña,
-      fecha_nacimiento,
-      fecha_ingreso,
-    };
-  }
-    ;
-
+      await apiPut("usuarios", data);
+      data = {
+        dni,
+        nombre,
+        apellido,
+        contraseña,
+        fecha_nacimiento,
+        fecha_ingreso,
+      };
+    }
     setShowModalUsuarioGuardar(true);
+    setShowRegistro(true)
     setDni("");
     setName("");
     setApellido("");
@@ -99,7 +136,7 @@ const Signup = (parametros) => {
       }
       return false;
     });
-    setUsuarios(resultadosBusqueda)
+    setUsuarios(resultadosBusqueda);
   };
 
   async function signUp() {
@@ -147,11 +184,12 @@ const Signup = (parametros) => {
       <div>
         <div className="containerInput">
           <input
-            className="form-control inputBuscar"
+            className="form-control inputBuscar d-inline"
             value={busqueda}
             placeholder="buscar por Nombre, apellido o dni"
             onChange={handleChange}
           />
+          <button href="#registro" onClick={mostrarRegistrar} className="btn btn-success .position-absolute top-0 end-0 " >Registrar Usuario</button>
         </div>
         <div className="table-responsive">
           <table className="table table-sm table-bordered">
@@ -168,7 +206,7 @@ const Signup = (parametros) => {
               {usuarios &&
                 usuarios.map((usuario) => (
                   <tr key={usuario.id}>
-                    <td >{usuario.id}</td>
+                    <td>{usuario.id}</td>
                     <td>{usuario.dni}</td>
                     <td>{usuario.nombre}</td>
                     <td>{usuario.apellido}</td>
@@ -182,6 +220,7 @@ const Signup = (parametros) => {
                         <i className="bi bi-pencil-square"></i>
                       </button>
                       <button
+                        onClick={() => modalRol(usuario) }
                         type="button"
                         className="btn btn-light m-1"
                         title="Agregar rango"
@@ -196,8 +235,8 @@ const Signup = (parametros) => {
         </div>
       </div>
 
-      <div className="col-sm-6 offset-sm-3 ">
-        <h1 className="titulo-reg">registrate</h1>
+      {!showResitro &&<div className="col-sm-6 offset-sm-3 ">
+        <h1 id="registrar" className="titulo-reg">registrate</h1>
         <h4>Numero de documento</h4>
         <input
           id="dni"
@@ -248,6 +287,7 @@ const Signup = (parametros) => {
           placeholder="Apellido"
         />
         <br></br>
+        
         {showModalUsuarioGuardar && <h4>Contraseña</h4>}
         {showModalUsuarioGuardar && (
           <input
@@ -259,14 +299,50 @@ const Signup = (parametros) => {
           />
         )}
         <br />
+        {showModalUsuarioGuardar && (
+          <button className="btn btn-outline-success" onClick={signUp}>
+            registrar
+          </button>
+        )}
+        {!showModalUsuarioGuardar && (
+          <button className="btn btn-outline-warning " onClick={guardarUsuario}>
+            Editar
+          </button>
+        )}
 
-        {showModalUsuarioGuardar && <button className="btn btn-outline-success" onClick={signUp}>
-          registrar
-        </button>}
-        {!showModalUsuarioGuardar && <button className="btn btn-outline-warning " onClick={guardarUsuario}>
-          Editar
-        </button>}
-      </div>
+        {/* formulario rol */}
+        {<h4>Rol del usuario</h4>}
+        
+        {!showModalRol &&<select  id="rol" className="form-select form-select-lg mb-3 " aria-label=".form-select-lg example">
+          <option value={"DEFAULT"}>Abrir para seleccionar rol</option>
+          <option value={1}>Sistema</option>
+          <option value={2}>Cajero</option>
+          <option value={3}>Repositor</option>
+          
+        </select>}
+        <br></br>
+        {!showModalRol &&<h4>Numero de documento</h4>}
+        {!showModalRol &&<input
+          id="dni-rol"
+          type="number"
+          value={dni}
+          onChange={(e) => setDni(e.target.value)}
+          className="form-control"
+          placeholder="D.N.I"
+        />}
+        <br />
+        {!showModalRol &&(
+          <button className="btn btn-outline-warning " onClick={singnUpRol}>
+            Agregar rol
+          </button>
+        )}
+        {!showModalRol &&(
+          <button className="btn btn-outline-warning " onClick={guardarRol}>
+            Editar rol
+          </button>
+        )}
+        <br></br>
+      </div>}
     </>
   );
 };
--- src/componentes/signup/TablaUsuarios.jsx
+++
@@ -1,5 +0,0 @@
-const tablaUsuarios = (parametros) =>{
-   
-
-}
-export {tablaUsuarios}
